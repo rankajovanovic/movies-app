37f38e19426a302f18856b8e98a30cbcf19deb45
Ranka <ranka.j.94@gmail.com>
2021-02-08 21:24:31 +0100

    nista ne radi kako treba

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+      <h2>Login</h2>
+       <div class="d-flex justify-content-center align-items-center container">
+          <form class="row g-3">
+            <div class="col-auto">
+              <label for="email" class="visually-hidden">Email</label>
+              <input type="email" class="form-control" id="email" value="" placeholder="Email">
+            </div>
+            <div class="col-auto">
+              <label for="password" class="visually-hidden">Password</label>
+              <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            <div class="col-auto">
+              <button type="submit" class="btn btn-primary mb-3">Login</button>
+            </div>
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
